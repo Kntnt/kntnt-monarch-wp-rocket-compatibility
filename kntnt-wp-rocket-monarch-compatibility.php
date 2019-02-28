c6bad3185a3479f58e8003c7e2451312f5440d37
Thomas Barregren <thomas@kntnt.se>
2019-02-28 10:38:02 +0100

    Some preening.

---
+++ kntnt-wp-rocket-monarch-compatibility.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt compatibility plugin for WP-Rocket and Monarch
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Exclude Monarch's CSS to be cached by WP-Rocket.
+ * Version:           1.0.1
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Kntnt\WP_Rocket_Monarch_Compatibility;
+
+defined('WPINC') || die;
+
+new Plugin;
+
+final class Plugin {
+
+    private $file;
+
+    public function __construct() {
+
+        $wp_dir = rtrim(strtr(ABSPATH, '\\', '/'), '/');
+        $site_dir = strtr($_SERVER['DOCUMENT_ROOT'], '\\', '/');
+        $wp_dir_rel_site = substr($wp_dir, strlen($site_dir));
+        $this->file = "$wp_dir_rel_site/wp-content/plugins/monarch/css/style.css";
+
+        add_filter('rocket_exclude_css', [$this, 'exclude']);
+        add_filter('rocket_exclude_cache_busting', [$this, 'exclude']);
+
+    }
+
+    private function exclude($exclude_files) {
+        $exclude_files[] = $this->file;
+        return $exclude_files;
+    }
+
+}
