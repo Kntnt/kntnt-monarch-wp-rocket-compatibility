ea0b1abee30275236f6256cffca48b5534d0ca49
Thomas Barregren <thomas@kntnt.se>
2019-08-23 15:57:23 +0200

    Updated.

--- kntnt-wp-rocket-monarch-compatibility.php
+++ kntnt-wp-rocket-monarch-compatibility.php
@@ -14,29 +14,29 @@
 
 namespace Kntnt\WP_Rocket_Monarch_Compatibility;
 
-defined('WPINC') || die;
+defined( 'WPINC' ) || die;
 
 new Plugin;
 
 final class Plugin {
 
-    private $file;
+	private $file;
 
-    public function __construct() {
+	public function __construct() {
 
-        $wp_dir = rtrim(strtr(ABSPATH, '\\', '/'), '/');
-        $site_dir = strtr($_SERVER['DOCUMENT_ROOT'], '\\', '/');
-        $wp_dir_rel_site = substr($wp_dir, strlen($site_dir));
-        $this->file = "$wp_dir_rel_site/wp-content/plugins/monarch/css/style.css";
+		$wp_dir = rtrim( strtr( ABSPATH, '\\', '/' ), '/' );
+		$site_dir = strtr( $_SERVER['DOCUMENT_ROOT'], '\\', '/' );
+		$wp_dir_rel_site = substr( $wp_dir, strlen( $site_dir ) );
+		$this->file = "$wp_dir_rel_site/wp-content/plugins/monarch/css/style.css";
 
-        add_filter('rocket_exclude_css', [$this, 'exclude']);
-        add_filter('rocket_exclude_cache_busting', [$this, 'exclude']);
+		add_filter( 'rocket_exclude_css', [ $this, 'exclude' ] );
+		add_filter( 'rocket_exclude_cache_busting', [ $this, 'exclude' ] );
 
-    }
+	}
 
-    public function exclude($exclude_files) {
-        $exclude_files[] = $this->file;
-        return $exclude_files;
-    }
+	public function exclude( $exclude_files ) {
+		$exclude_files[] = $this->file;
+		return $exclude_files;
+	}
 
 }
